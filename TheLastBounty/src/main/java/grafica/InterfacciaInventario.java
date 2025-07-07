30356fa952032d51c97caa633089ad482037b548
marzullidino <marzullidino@gmail.com>
2025-07-07 20:59:55 +0200

    inventario

--- TheLastBounty/src/main/java/grafica/InterfacciaInventario.java
+++ TheLastBounty/src/main/java/grafica/InterfacciaInventario.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ 
 package grafica;
 
 import java.awt.BorderLayout;
@@ -134,3 +134,4 @@ public class InterfacciaInventario extends javax.swing.JFrame {
         setLocationRelativeTo(null); 
     }
 }
+*/
